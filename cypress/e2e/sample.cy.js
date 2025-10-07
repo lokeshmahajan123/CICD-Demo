8090973f7569d8d13f3b89ba4b435d5094fc2c4f
Lokesh Mahajan <lokeshmahajan1133@gmail.com>
2025-10-08 01:57:16 +0530

    Initial commit with unit and e2e tests

--- cypress/e2e/sample.cy.js
+++ cypress/e2e/sample.cy.js
@@ -1,6 +1,6 @@
 describe('Cypress Example Test', () => {
   it('visits React app homepage', () => {
-    cy.visit('http://localhost:3000');   // app run hone ke baad open karega
-    cy.contains('Learn React');          // check karega ki ye text hai ya nahi
+    cy.visit('http://localhost:3000/');   // app run hone ke baad open karega
+    cy.contains('My React App');          // check karega ki ye text hai ya nahi
   });
 });
